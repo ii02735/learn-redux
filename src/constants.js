e12ebf77710f8482bede8535f03f571848103776
ii02735 <bilaal.yadallee@gmail.com>
2020-08-23 14:00:27 +0200

    [ACTIONS] Structure pour les actions

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+//Définition des constantes pour les différentes actions
+const constants = {
+    ADD_BOOK: "ADD_BOOK",
+    REMOVE_BOOK: "REMOVE_BOOK",
+    ADD_ERROR: "ADD_ERROR",
+    BORROW_BOOK: "BORROW_BOOK", //pour réserver / emprunter un livre
+    GIVE_BACK_BOOK: "GIVE_BACK_BOOK" //rendre un livre
+}
+
+//On définit des constantes pour obliger JS à choisir des actions existantes que de taper des string
+
+
+export default constants;
+
