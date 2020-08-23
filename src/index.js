a9ec4c9b313bb31d0ff2e7d6a6a969e0c8d143b2
ii02735 <bilaal.yadallee@gmail.com>
2020-08-23 14:53:34 +0200

    Affichage des données de states dans index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 /**
  * Il nous est possible d'utiliser la syntaxe ES6 grâce à Babel
+ * Par conséquent, le fichier index.js sera indiqué comme étant un module ES
+ * Un Module ES est pratique pour pouvoir exposer des fonctionnalités à d'autres fichiers
  * Et pour dire à Babel qu'il faudra lancer ce script, il faudra modifier le package.json
+ * 
  */
 import CONSTANTS from "./constants"
 import { allBooks, booksBorrowed } from "./initialState.json"
 
 console.log(`
     Voici les livres :
-    ${allBooks}
+    ${allBooks.map((book) => book.titre).join(', ')}
+
+    Ainsi que les actions :
+    ${Object.keys(CONSTANTS).join('\n       ')}
 `)
