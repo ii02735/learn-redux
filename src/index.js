6ec7b20376e7e27b2d93272fed65a2bc492a03d8
ii02735 <bilaal.yadallee@gmail.com>
2020-08-24 03:02:16 +0200

    [REDUX] Middleware

--- src/index.js
+++ src/index.js
@@ -11,77 +11,77 @@
  //Il est inutile de passer par le mainReducer manuellement pour gérer / muter notre état
  //C'est justement le STORE qui gère la mutation d'état 
 
- import { createStore } from "redux"
-
- 
- const store = createStore(mainReducer, initialState); //création d'un store
-                              //On récupère l'état actuel du store (soit l'état global)
-                              /**
-                               * Chaque valeur du state est la valeur par défaut renseignée
-                               * dans les fonctions des reducers
-                               */
-//grâce au createStore, il est inutile d'appeler le mainReducer et de créer des variables différentes qui vont stocker les différents states
-//getState() permet donc de récupérer l'état global
- console.log("Initial state", store.getState());
+ import { createStore, applyMiddleware } from "redux"
 
  /**
-  * Pour MUTER un état, on utilise la fonction DISPATCH
-  */
-  
-  //Exemple pour muter le compteur
-  /**
-   * dispatch contacte les reducers, et le bon reducer (d'après le type)
-   * procèdera au changement de l'état
-   */
-  store.dispatch({
-     type: CONSTANTS.SET_COMPTEUR,
-     payload: 2000
-  })
-
- /**
-  * Il est possible d'exécuter une callback à chaque fois après
-  * que le store / le state est modifié
-  * à l'aide de la fonction SUBSCRIBE
+  *  Les middlewares
+  *  
+  *  Les middlewares sont des concepts importants qui peuvent
+  *  modifier le cheminement de l'exécution d'une action
+  *  
+  *  On peut donc exécuter de la logique avant que la modification d'état ne se produise
+  *  ou après
   * 
-  * Ainsi on ajoute un écouteur à redux qui va gérer un événement
-  * 
-  * Attention, l'ordre est critique
-  * Il faut que cette fonction soit définie AVANT la modification du store
-  */
+  *  Si on souhaite par exemple, logger les différentes valeurs du store avant et après un changement
+  */ 
 
-  const listener = store.subscribe(() => console.log("Hey the store has been updated !", store.getState()))
+ const loggerMiddleware = (store) => {
+    //Ce bloc s'exécute dès création de loggerMiddleware
+    //On peut accéder au store ici
+    console.info("Création Logger Middleware")
+    return (next) => {
+       //ici on accès à la fonction qui va dispatch au store
+       //ce n'est pas trop utile
+       return (action) => {
+         //La partie la plus intéressante est ce bloc
+         console.groupCollapsed("PRÉPARATION DISPATCH")
+         console.warn("Action demandée : ",action.type)
+         console.log("État actuel du store", store.getState())
+         console.groupEnd();
+         //Ici a lieu l'exécution de l'action avec le dispatch
+         //grâce à l'instruction suivante :
+         const result = next(action);
+         //Toute instruction après le next sera donc forcément exécuté après la réalisation du dispatch
+         //Et toute instruction AVANT, sera exécuté avant réalisation du dispatch
+         console.groupCollapsed("DISPATCH EXÉCUTÉ")
+         console.log("Nouvel état du store : ", store.getState())
+         console.groupEnd();
+         
+         //Les middlewares sont importants, car ils peuvent exécuter du code APRÈS changement de states
+         //Ou même avant, ce qui est utile pour gérer de l'asynchronicité (dans des promesses, on fait appel à des dispatch, ce qui
+         //va entrer à nouveau dans le cycle du middleware !)
+         
+         //Pour s'assurer que le changement de l'état soit bien pris en compte, il faut retourner le résultat du dispatch
 
-  store.dispatch({
-    type: CONSTANTS.BORROW_BOOK,
-    payload: store.getState().allBooks.filter((book) => book.titre === "Le républicain vert")[0]
-  });
+         return result;
+       }
+    }
+ } 
 
-  store.dispatch({
-     type: CONSTANTS.ADD_BOOK,
-     payload: {
-        id: 6,
-        titre: "Redux et RxJS",
-        auteur: "Abramov",
-        sortie: "2018",
-        genre: "Développement web"
-     }
-  })
 
-  //Il est possible de UNSUBSCRIBE (désincrire) un événement on invoquant l'objet qui a stocké le résultat du subscribe
-  
-  listener();
+ //Il faut ATTACHER le middleware à notre store afin que ce dernier soit rajouté dans l'exécution des dispatch de ces derniers
 
-  //À ce stade là, le dispatch ci-dessous n'invoquera plus la callback du listener
 
-  store.dispatch({
-   type: CONSTANTS.ADD_BOOK,
-   payload: {
-      id: 6,
-      titre: "Redux et API Context",
-      auteur: "Abramov",
-      sortie: "2020",
-      genre: "Développement web"
-     }
-  })
 
-  console.log("New books : ", store.getState().allBooks)
+const store = applyMiddleware(loggerMiddleware)(createStore)(/** arguments de createStore */mainReducer,initialState)
+
+store.dispatch({
+   type: CONSTANTS.SET_COMPTEUR,
+   payload: 1000
+});
+
+store.dispatch({
+   type: CONSTANTS.BORROW_BOOK,
+   payload: store.getState().allBooks.filter((book) => book.titre === "Le républicain vert")[0]
+ });
+
+ store.dispatch({
+    type: CONSTANTS.ADD_BOOK,
+    payload: {
+       id: 6,
+       titre: "Redux et RxJS",
+       auteur: "Abramov",
+       sortie: "2018",
+       genre: "Développement web"
+    }
+ })
